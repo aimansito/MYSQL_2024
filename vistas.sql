df8cfd7fec4bc209973049de612352e68dbd2118
Aiman <ahardao1001@g.educaand.es>
2024-04-11 14:44:31 +0200

    c2

--- vistas.sql
+++ vistas.sql
@@ -1,25 +1,24 @@
 use ventapromoscompleta;
 DROP VIEW IF EXISTS articulosActuales;
 CREATE VIEW articulosActuales
-	(codart,nomart,precioart)
+	(codart,nombreart,precioart)
 AS 	
 	SELECT articulos.refart, nomart, precioartpromo as Articulos
     FROM articulos
     JOIN catalogospromos on articulos.refart = catalogospromos.refart 
     JOIN promociones on catalogospromos.codpromo = promociones.codpromo
-    WHERE curdate() between promociones.fecinipromo and date_add(promociones.fecinipromo, interval promociones.duracionpromo day)
-    UNION 
+    WHERE curdate() between promociones.fecinipromo and date_add(promociones.fecinipromo, interval promociones.duracionpromo day);
+  /*  UNION 
     SELECT articulos.refart , articulos.nomart, articulos.precioventa 
     FROM articulos 
     WHERE refart not in (SELECT articulos.refart, nomart, precioartpromo as Articulos
     FROM articulos
     JOIN catalogospromos on articulos.refart = catalogospromos.refart 
     JOIN promociones on catalogospromos.codpromo = promociones.codpromo
-    WHERE curdate() between promociones.fecinipromo and date_add(promociones.fecinipromo, interval promociones.duracionpromo day));
+    WHERE curdate() between promociones.fecinipromo and date_add(promociones.fecinipromo, interval promociones.duracionpromo day));*/
     
-select precio from articulosActuales 
-where codigo = 'C1_01'
-;
+select * from articulosActuales ;
+select * from articulos;
 
 -- para la badat d test codtest y codpregunta que este repetida
 
